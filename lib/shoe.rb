9d3ade49a3a9dab648782b0ff0a0b47104be7e8f
simque <simon.duncanson@gmail.com>
2015-11-29 11:47:25 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,17 @@
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.any?{|element| element == brand}
+      BRANDS
+    else
+      BRANDS << brand
+    end
   end
 
   def cobble
