341a0ea0d244f4a89f6d7a081bb114fbcaf5d2cb
Mitul Mistry <mitul.mistry1@gmail.com>
2016-01-21 22:12:49 +0000

    nitrous-backup at Thu Jan 21 22:12:49 UTC 2016

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
-  BRANDS = []
+
+  BRANDS = [] #class constant, all caps
 
   def initialize(brand)
     @brand = brand
