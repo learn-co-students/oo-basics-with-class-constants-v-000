d5cb5cc69fb02eebc5211002ce4af8a397809973
reifnotreef <bridgerrhammond@gmail.com>
2016-02-01 14:43:51 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,19 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
   def initialize(title)
     @title = title
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @grenre = genre
+    GENRES << genre
   end
 
 end
