dd225384ea1a9a7ebb3af3a7fc89d22e2f59c914
marcelgalang <marcelgalang@gmail.com>
2016-02-20 12:55:00 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,6 +2,13 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
   def initialize(title)
     @title = title
   end
