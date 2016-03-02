bc3daf7fe7ac7243359f8183bb02d6c13fd54e26
rafeeq-123 <rafeeq-123@hotmail.com>
2016-03-02 13:18:59 -0800

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
+  attr_reader :brand 
+BRANDS = []
   def initialize(brand)
     @brand = brand
+    BRANDS.include?(brand) ? nil : BRANDS << brand
+ 
+
   end
 
   def cobble
@@ -11,4 +14,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+ end
+
+
 end
+ 
