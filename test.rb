baccfc4d41d0d65d84958bce4609390117fb26fb
nOraKat147 <n.mori.iphone@gmail.com>
2016-04-18 14:09:58 -0400

    Done.

--- test.rb
+++ test.rb
@@ -12,8 +12,32 @@ class Book
 end
 
 
-asd = Book.new('yum')
+#asd = Book.new('yum')
+#binding.pry
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  BRANDS = []
+
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
+    puts BRANDS
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
+
+
+new1 = Shoe.new('test')
+
 binding.pry
-
-
-
+new2 = Shoe.new('test2')
+binding.pry
+new3 = Shoe.new("test")
+binding.pry
