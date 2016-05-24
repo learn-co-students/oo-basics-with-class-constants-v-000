ace8df3a08353930f78496f470bd958cdd6c0a75
hilaryml <hilyttle@gmail.com>
2016-05-24 16:03:57 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre  #this will generate a reader/getter for us
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,4 +12,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)  #this will assign genre to an instance variable of @genre, and will shovel genre into the class constant GENRE array
+  	@genre = genre
+  	GENRES << genre
+  end
 end
