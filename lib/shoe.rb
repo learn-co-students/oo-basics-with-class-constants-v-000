7202668784dab1ee9a281f8f6ac0156747ebfee7
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-07 17:27:35 +0000

    completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand) #check if the brand is not already in the array if it is don't add the duplicate else add it
     BRANDS << brand
+  end
   end
 
  #to store and keep track of all brands
