75cc180d1dd14304e58bdc15f7d7cb385e6edf4e
William Jones <enquiries@forestfed.com>
2016-07-25 18:42:04 +0000

    use class constants

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,18 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+  # create the writer for brand and add the logic for the class constant
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end
 
 end
