6ff470a5c6d0b5dce531c58867a1f2c084469735
Elias Ventura <eliasventura23@gmail.com>
2016-08-09 20:59:36 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count
-  attr_reader :title, :genre
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
   GENRES = []
 
@@ -13,7 +13,7 @@ class Book
   end
 
   def genre=(genre)
-    @genre = genre
     GENRES << genre
   end
-    end
+
+end
