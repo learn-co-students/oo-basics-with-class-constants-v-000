d695ad2b6eeef2e954315327d18db8a9fd43f8d9
Samir Singh <singhstart@gmail.com>
2016-09-01 17:59:23 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,16 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre # assigns parameter to instance variable
+    GENRES << genre # adds genre to class constant
   end
 
   def turn_page
