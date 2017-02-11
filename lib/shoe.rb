ad12acf4a08ddd38a75f5082bd97511ae4b89083
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-11 15:08:49 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,20 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = [] #estan;osjes BRANDS as an empty array CONSTANT
+
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+    BRANDS << brand
+  end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+
 
 end
