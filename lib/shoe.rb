15b76603f012c7a2d09b092f4b050be21f5bc801
jose cobos <luiscobosj@gmail.com>
2017-04-11 20:29:48 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  BRANDS = []
   def initialize(brand)
     @brand = brand
+    !BRANDS.include?(brand) ? BRANDS << brand : "Brand Already exists"
   end
+
+
 
   def cobble
     self.condition = "new"
