ca26b09177c02d5f5a2b1e342629bff39c68562c
Joseph Hyatt <jhyatt1017@gmail.com>
2017-04-16 16:35:12 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,18 @@
+#Shoe BRANDS keeps track of all brands
+#Shoe BRANDS only keeps track of unique brands
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
