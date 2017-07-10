a2ce5005414a1aaa5e71fde1679c3ab81f67ac9a
Dalia Sawaya <sawaya.dalia@gmail.com>
2017-07-10 22:09:48 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,25 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+  AUTHORS = []
+
   def initialize(title)
     @title = title
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
+  def author=(author)
+    @author = author
+    AUTHORS << author
   end
 
 end
