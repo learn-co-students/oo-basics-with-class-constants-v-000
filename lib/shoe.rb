7c4bac362f09ceb69cff24af559bd6601e073b60
kelly <github email address>
2017-09-21 09:36:31 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []#set constant = empty array
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)#add brand unless present in array BRANDS
   end
 
   def cobble
@@ -11,4 +14,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-end
+ end
+ genres = ["Thriller", "Science Fiction", "Romance"]
+      genres.each_with_index do |genre, i|
+        book = Book.new("Book_#{i}")
+        book.genre = genre
+      end
