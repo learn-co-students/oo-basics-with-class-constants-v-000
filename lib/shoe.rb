6da3fd01f802fa53beee3eba4304f0abda6e9094
zolkocarbon <eathena@hotmail.com>
2017-10-05 14:41:16 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand if !BRANDS.include?(brand)
+    #add to array only if it's unique
   end
 
   def cobble
