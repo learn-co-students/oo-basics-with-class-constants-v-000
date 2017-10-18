6dbe1092d735c54c90bf4282ffe1986741d27506
Christopher DiStasio <cadistasio@gmail.com>
2017-10-18 18:27:09 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,13 +6,16 @@ BRANDS = []
 
   def initialize(brand)
     @brand = brand
+  end
+
+  def brand=(brand)
+    @brand = brand
     brand.each_with_index do |brand_of_shoe_element, index|
       if index/1 == 1
       BRANDS << brand
       end
     end
   end
-
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
