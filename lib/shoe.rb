d0e14a6834af1f24914ba45707cda5b69520ed03
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-22 16:13:24 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,19 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    unique_brand = true
+    BRANDS.each do |b|
+      if b == brand
+        unique_brand = false
+      end
+    end
+    if unique_brand
+      BRANDS << brand
+    end
   end
 
   def cobble
