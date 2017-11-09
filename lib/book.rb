a9011e2f760785faed6e739b2b22d9f1aac6f3d8
Elle Faraday <faraday.elle@gmail.com>
2017-11-09 02:28:20 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,11 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  def genre= (genre)
+    @genre = genre
+    GENRES << genre
+  end
 
   def initialize(title)
     @title = title
@@ -9,5 +14,7 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+  GENRES = []
 
 end
