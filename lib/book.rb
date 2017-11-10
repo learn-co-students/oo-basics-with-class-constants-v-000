ef7c21b3dcfcf7a5947392c26f419f35ec5c84d5
charityyoungblood <youngblood.charity@gmail.com>
2017-11-10 14:12:04 -0800

    added setter method for genre to book.rb

--- lib/book.rb
+++ lib/book.rb
@@ -4,12 +4,16 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  GENRES = [@title]
+  GENRES = [@genre]
 
   def initialize(title)
     @title = title
   end
-
+  
+  def genre=(book_genre)
+    @genre = book_genre
+  end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
