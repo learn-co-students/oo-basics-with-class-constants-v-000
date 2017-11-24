c10431c24460984d2edaf52175696b077f23423f
cboujoukos <cboujoukos@gmail.com>
2017-11-24 11:01:45 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,18 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre
+    if !GENRES.include?(genre)
+      GENRES << genre
+    end
   end
 
   def turn_page
