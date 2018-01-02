71aac6c36e393297000dab8b8260ed91f42c74de
Laura <laurao148@gmail.com>
2018-01-02 04:21:06 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,16 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+  BRANDS_NEW = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+  end
+
+  def brand=(brand)
+    BRANDS.length - 1
   end
 
   def cobble
