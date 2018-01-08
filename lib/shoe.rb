454a8b6c49225abc53e0617e4ec5d83aaa65a139
EPIC448 <samrey2018@gmail.com>
2018-01-08 17:23:03 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,6 +9,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     #if brand is not included in BRANDS ... This control for the uniquness of the brand.
+    #       ! makes is a  if brand is not included..
     if !BRANDS.include?(brand) # Hey, is this brand in the BRAND array. It will either be true or false.
       #SHovel it into BRANDS
       BRANDS << brand # equivalent to BRANDS.push(brand)
@@ -16,7 +17,7 @@ class Shoe
   end
 
   def cobble
-    self.condition = "new" # I want to call the Shoe#condition method 
+    self.condition = "new" # I want to call the Shoe#condition method
     condition = "new" # I want to assign the string "new" to a variable called condition
     puts "Your shoe is as good as new!"
   end
