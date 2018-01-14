740fa9bc9fc9c2f959fc2b385d86a6f580f5001d
churchwerks <steven.lennis@icloud.com>
2018-01-14 09:22:02 -0600

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -14,5 +13,5 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
+
 end
