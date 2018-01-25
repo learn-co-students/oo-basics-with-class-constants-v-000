ae19867d9b0d869261e829b2aface039942f283c
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-25 13:46:04 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.exclude?(brand) do
+    if !BRANDS.include?(@brand)
       BRANDS << brand
     end
   end
@@ -15,7 +15,5 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-
 
 end
