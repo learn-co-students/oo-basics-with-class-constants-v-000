cf6f94263a40c74bd5a8744af5d61bc4354f59e8
Harley Rayner <harleymteden@gmail.com>
2018-02-12 23:41:57 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -6,11 +8,16 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.include? @brand
+    else
+      BRANDS << @brand
+    end
   end
+
+  # binding.pry
 
   def brand=(brand)
     @brand = brand
-    BRANDS << brand
   end
 
   def cobble
