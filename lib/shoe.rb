ef27f09d68aa52eba1bc21ff21ba009fe2a2cd0d
Brandon Chin <brandonchin@me.com>
2018-03-02 23:19:45 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand) == false 
+    if BRANDS.include?(brand) == false
       BRANDS << brand
     end
 
@@ -16,6 +16,5 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 
 end
