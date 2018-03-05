6b794e0652024a8638caed9e90bcac2e7093c225
Katie Cassell <kcassell944@gmail.com>
2018-03-05 18:06:30 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,17 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+
+
+    # if the brand is in the array of BRANDS
+      # dont add it
+    if !BRANDS.include?(brand)
+      BRANDS << brand # add the brand to BRANDS
+    end
   end
 
   def cobble
