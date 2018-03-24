3b5618c8e1f3c839b2036910378581a13b8dc88c
Erica Murdock-Waters <ericahpa@gmail.com>
2018-03-24 06:42:54 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,17 @@
+# shoe.rb
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+   attr_reader :brand
 
+  BRANDS = [] # def class constant BRANDS
+
+  # constructor method
   def initialize(brand)
-    @brand = brand
+    @brand = brand # assign instance variable @brand
+    BRANDS << brand unless BRANDS.include?(brand)
+    # alternate for rb: 12 is rb: 14
+    # BRANDS << brand if BRANDS.include?(brand) == false
   end
 
   def cobble
