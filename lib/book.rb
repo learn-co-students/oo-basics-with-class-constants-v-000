fe5f95b7a6edc8ab72ee81d4777811af78bf765b
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-28 16:54:19 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,19 +1,21 @@
 class Book
-  attr_accessor :author, :page_count
-  attr_reader :title, :genre
+attr_accessor :author, :page_count
+attr_reader :title, :genre
 
-  GENRES = []
+GENRES = []
 
-  def initialize(title)
-    @title = title
-  end
+def initialize(title)
+  @title = title
+end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
 
-  def genre=(genre)
-    @genre = genre
-    GENRES << genre
-  end
+def genre=(genre)
+  @genre = genre
+  GENRES << genre
+end
+
+
 end
