829c40bc969e61e17f63ad12475c55e75def0390
Isaac Villicana <villicana.i@gmail.com>
2018-04-12 13:47:21 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand if BRANDS.none?{|a| a == brand}
   end
 
   def cobble
