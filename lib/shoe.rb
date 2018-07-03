484284db773599237e0043d9cf3d245d9c8ab90a
Justine Bricknell <justinegayton@gmail.com>
2018-07-03 13:54:00 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.include?(brand)
+    #BRANDS << brand unless BRANDS.include?(brand)
+    BRANDS << brand unless BRANDS.any? { |brand| brand == brand }
   end
 
   def cobble
