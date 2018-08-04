f3376901683635251edfb592a0fbdb092dd189c6
Niusha H <niusha.hadziomerovic@gmail.com>
2018-08-04 22:13:03 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
+require "pry"
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  require 'set'
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand if !BRANDS.include?(brand)
   end
 
   def cobble
