c34ae1c004bbff6797861d26953a04ca27669598
Steven Crouse <sdcrouse91@gmail.com>
2018-08-28 07:18:35 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,20 @@
+require "pry"
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    
+    unless BRANDS.include?(brand)
+      BRANDS << brand
+    end
+    #binding.pry
+    # Also, I'm not sure which is faster: 
+    # Using the code above, or deleting duplicate genres from GENRES
   end
 
   def cobble
