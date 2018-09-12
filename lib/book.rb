b80fcd8e941f499c8e5414ee36d8aa3f2319809c
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-12 22:10:53 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,26 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+
+  GENRES = Array.new
+
+  attr_accessor :author, :page_count
   attr_reader :title
+  attr_reader :genre
 
   def initialize(title)
     @title = title
   end
 
+  def genre=(genre)
+    GENRES.push(genre)
+  end 
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+  def genres
+    GENRES
+  end
+
 
 end
