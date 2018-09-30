4ca3ecf5128281fad79a45d58717db0dacc72240
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-30 00:25:54 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -16,25 +16,3 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 end
-
-
-
-# class Book
-#   attr_accessor :author, :page_count
-#   attr_reader :title
-  
-#   GENRES = []
-
-#   def initialize(title)
-#     @title = title
-#   end
-  
-#   def turn_page
-#     puts "Flipping the page...wow, you read fast!"
-#   end
-
-#   def genre=(genre)
-#     @genre = genre
-#     GENRES << genre 
-#   end 
-# end
