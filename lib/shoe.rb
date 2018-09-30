80fb4d6a1b2079b7497216c44bb4d2f6a4355b2f
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-30 00:25:37 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,40 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(@brand)
+      BRANDS << @brand
+    end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
+
+
+
+# class Book
+#   attr_accessor :author, :page_count
+#   attr_reader :title
+  
+#   GENRES = []
+
+#   def initialize(title)
+#     @title = title
+#   end
+  
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+
+#   def genre=(genre)
+#     @genre = genre
+#     GENRES << genre 
+#   end 
+# end
