5d9128d9ffc81fcc7ba1913f3fc327b40e6a8f4e
ktochow1 <ktochow1@binghamton.edu>
2018-11-01 16:12:39 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,21 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :material, :condition, :size
   attr_reader :brand
-
+  
+  BRANDS = []
+  
   def initialize(brand)
     @brand = brand
+    #BRANDS << @brand
+    BRANDS << brand unless BRANDS.include?(brand)
+      if !BRANDS.include?(@brand)
+        BRANDS << @brand
+    end
   end
+
+  # you would check to see if "brand" is inside of "BRANDS" - if not, go ahead and add it - if so, ignore it
+
+  
 
   def cobble
     self.condition = "new"
@@ -12,3 +23,6 @@ class Shoe
   end
 
 end
+
+
+
