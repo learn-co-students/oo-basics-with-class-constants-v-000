57f1789e526bd8b335a4b2f2f229dc5f74606aa1
Brad Schrag <brad.schrag@gmail.com>
2018-11-12 02:30:46 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,22 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
+
+  GENRES = ["Thriller", "Science Fiction", "Romance"]
 
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre
+    if !GENRES.include?(genre)
+      GENRES << genre
+    end
+  end
+
+  def genre
+    @genre
   end
 
   def turn_page
