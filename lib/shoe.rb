34c2820a1143ae1790777486830d87ccb95732dd
Christian V <viray.christianj@gmail.com>
2018-11-17 19:39:10 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,39 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+  
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS == BRANDS.uniq!
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+end
+
+
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre 
+
+  GENRES = []
+  
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+    def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end 
 
 end
